315d932a5369fd1e2fec566da511d274e8c407f5
subzero <dima@svalov.ru>
2016-05-22 22:54:23 +0300

    GenPlan QS

--- genplan/src/main/java/ru/svalov/ma/genplan/model/TrelloCard.java
+++ genplan/src/main/java/ru/svalov/ma/genplan/model/TrelloCard.java
@@ -2,15 +2,18 @@ package ru.svalov.ma.genplan.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.Date;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Project {
+public class TrelloCard {
 
     private String id;
     private String name;
     private String desc;
+    private String idBoard;
     private String idList;
+    private Date due;
     private List<String> idLabels;
 
     public String getId() {
@@ -37,12 +40,28 @@ public class Project {
         this.desc = desc;
     }
 
+    public String getIdBoard() {
+        return idBoard;
+    }
+
+    public void setIdBoard(String idBoard) {
+        this.idBoard = idBoard;
+    }
+
     public String getIdList() {
         return idList;
     }
 
     public void setIdList(String idList) {
         this.idList = idList;
+    }
+
+    public Date getDue() {
+        return due;
+    }
+
+    public void setDue(Date due) {
+        this.due = due;
     }
 
     public List<String> getIdLabels() {
--- genplan/src/test/java/ru/svalov/ma/genplan/QuickStartTest.java
+++ genplan/src/test/java/ru/svalov/ma/genplan/QuickStartTest.java
@@ -9,8 +9,9 @@ import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import org.testng.annotations.Test;
-import ru.svalov.ma.genplan.model.Project;
+import ru.svalov.ma.genplan.model.TrelloCard;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Properties;
 
@@ -24,30 +25,63 @@ public class QuickStartTest extends AbstractTestNGSpringContextTests {
     @Autowired
     private Properties testProperties;
 
-    @Test(enabled = false)
+    @Test(enabled = true)
     public void testInvokeTrello() {
 
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(
-                testProperties.getProperty("qst.url")
+                testProperties.getProperty("gp.board.url")
         )
                 .queryParam("key", testProperties.getProperty("app.key"))
                 .queryParam("token", testProperties.getProperty("app.token"));
 
-        // fields=name,idList,url&
-        ResponseEntity<List<Project>> projects = restTemplate.exchange(
+        ResponseEntity<List<TrelloCard>> projects = restTemplate.exchange(
                 builder.build().encode().toUri(),
                 HttpMethod.GET,
                 null,
-                new ParameterizedTypeReference<List<Project>>() {
+                new ParameterizedTypeReference<List<TrelloCard>>() {
                 }
         );
 
         System.out.println(projects);
     }
 
-    @Test(enabled = false)
+    @Test(enabled = true)
     public void testAddCard() {
 
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(
+                testProperties.getProperty("gp.cards.url")
+        )
+                .queryParam("key", testProperties.getProperty("app.key"))
+                .queryParam("token", testProperties.getProperty("app.token"));
+
+        TrelloCard card = new TrelloCard();
+        card.setIdBoard(testProperties.getProperty("gp.board.id"));
+        card.setIdList(testProperties.getProperty("gp.list.id"));
+        card.setDesc("test desc");
+        card.setName("test name");
+        card.setDue(new Date());
+
+        TrelloCard ret = restTemplate.postForObject(
+                builder.build().encode().toUri(),
+                card,
+                TrelloCard.class
+        );
+
+        UriComponentsBuilder builderComments = UriComponentsBuilder.fromHttpUrl(
+                testProperties.getProperty("gp.cards.url")
+                        + "/"
+                        + card.getId()
+                        + "/actions/comments"
+        )
+                .queryParam("text", "comment 1");
+        Object test = restTemplate.exchange(
+                builderComments.build().encode().toUri(),
+                HttpMethod.POST,
+                null,
+                Object.class
+        );
+
+        System.out.println(ret);
     }
 
 }
