c129f45dfc305345534fb932fd2122aaeacd8596
Subzero <dima@svalov.ru>
2017-12-05 12:35:54 +0300

    refactor ProgressStatusReport

--- genplan/src/main/java/ru/svalov/ma/progressreport/ProgressStatusServiceImpl.java
+++ genplan/src/main/java/ru/svalov/ma/progressreport/ProgressStatusServiceImpl.java
@@ -8,7 +8,9 @@ import org.springframework.web.client.RestTemplate;
 import ru.svalov.ma.progressreport.config.Board;
 import ru.svalov.ma.progressreport.model.ProgressReportItem;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ProgressStatusServiceImpl implements ProgressService {
 
@@ -29,30 +31,28 @@ public class ProgressStatusServiceImpl implements ProgressService {
                 }
         );
 
-        // todo : refactor
+        int undoneS = 0, undoneM = 0, undoneL = 0;
+        int doneS = 0, doneM = 0, doneL = 0;
 
-        int undoneS = 0;
-        int undoneM = 0;
-        int undoneL = 0;
-        int doneS = 0;
-        int doneM = 0;
-        int doneL = 0;
+        boolean done = false;
         for (ProgressReportItem item : re.getBody()) {
-
-            if (board.getDone().getId().equals(item.getIdList())) {
-                if (item.getIdLabels().contains(board.getSmall().getId())) {
+            done = board.getDone().getId().equals(item.getIdList());
+            if (item.getIdLabels().contains(board.getSmall().getId())) {
+                if(done) {
                     doneS++;
-                } else if (item.getIdLabels().contains(board.getMiddle().getId())) {
-                    doneM++;
-                } else if (item.getIdLabels().contains(board.getLarge().getId())) {
-                    doneL++;
-                }
-            } else if (item.getIdLabels().contains(board.getWeek().getId())) {
-                if (item.getIdLabels().contains(board.getSmall().getId())) {
+                } else {
                     undoneS++;
-                } else if (item.getIdLabels().contains(board.getMiddle().getId())) {
+                }
+            } else if (item.getIdLabels().contains(board.getMiddle().getId())) {
+                if(done) {
+                    doneM++;
+                } else {
                     undoneM++;
-                } else if (item.getIdLabels().contains(board.getLarge().getId())) {
+                }
+            } else if (item.getIdLabels().contains(board.getLarge().getId())) {
+                if(done) {
+                    doneL++;
+                } else {
                     undoneL++;
                 }
             }
@@ -60,18 +60,12 @@ public class ProgressStatusServiceImpl implements ProgressService {
 
         // build report text
         StringBuilder builder = new StringBuilder();
-        TextUtils.append(builder, "Undone S");
-        TextUtils.append(builder, String.valueOf(undoneS));
-        TextUtils.append(builder, "Undone M");
-        TextUtils.append(builder, String.valueOf(undoneM));
-        TextUtils.append(builder, "Undone L");
-        TextUtils.append(builder, String.valueOf(undoneL));
-        TextUtils.append(builder, "Done S");
-        TextUtils.append(builder, String.valueOf(doneS));
-        TextUtils.append(builder, "Done M");
-        TextUtils.append(builder, String.valueOf(doneM));
-        TextUtils.append(builder, "Done L");
-        TextUtils.append(builder, String.valueOf(doneL));
+        TextUtils.append(builder, "-------");
+        TextUtils.append(builder, "Undone");
+        TextUtils.append(builder, String.format("S: %d, M: %d, L: %d", undoneS, undoneM, undoneL));
+        TextUtils.append(builder, "-------");
+        TextUtils.append(builder, "Done");
+        TextUtils.append(builder, String.format("S: %d, M: %d, L: %d", doneS, doneM, doneL));
 
         return builder.toString();
     }
